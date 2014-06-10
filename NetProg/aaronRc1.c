9d0780b6d5eb1bda9f7f805a88ca7327778a4bea
rice2007 <rice2007@gmail.com>
2014-06-10 10:46:32 -0500

    Initial server/client build

---
+++ NetProg/aaronRc1.c
@@ -0,0 +1,8 @@
+/*
+ * aaronRc1.c
+ *
+ *  Created on: Jun 10, 2014
+ *      Author: Ajay
+ */
+
+
---
+++ NetProg/aaronRs1.c
@@ -0,0 +1,8 @@
+/*
+ * aaronRs1.c
+ *
+ *  Created on: Jun 10, 2014
+ *      Author: Ajay
+ */
+
+
