e98ff37608b3014d9ab343189147802bd99a63f3
rice2007 <rice2007@gmail.com>
2014-06-10 23:56:06 -0500

    Update

--- NetProg/aaronRs1.c
+++ NetProg/aaronRs1.c
@@ -8,14 +8,17 @@
 int main(int argc, char *argv[]) {
 
 	char buffer[1024];
-	int listen, connfd;
+	int listenfd, connfd;
 	struct sockaddr_in serv_addr;
 
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = htons(4096);
 	serv_addr.sin_port = htons(5000);
+	bind(listenfd, )
+	listen(listenfd, 5);
 
 	while(1) {
+		//connfd = accept(listenfd, (struct sockaddr*)NULL, NULL); 
 		write();
 	}
 }
