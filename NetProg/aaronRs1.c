7c7f80aad5cc9c48a9b490a1530805ee8d7415ca
rice2007 <rice2007@gmail.com>
2014-06-19 14:08:06 -0500

    Persistent client connection.

--- NetProg/aaronRs1.c
+++ NetProg/aaronRs1.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <strings.h>
+#include <sys/time.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -13,36 +14,84 @@
 int main(int argc, char *argv[]) {
 
 	char buffer[1024];
-	int server_socket, connfd;
+	fd_set fds;
+	int listenfd, connfd;
+	// int nread, nready, ns1, ns2, reader;
 	struct sockaddr_in serv_addr;
 
-	server_socket = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_socket < 0) {
-		printf("server sock: %d\n", server_socket);
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (listenfd < 0) {
+		printf("server sock: %d\n", listenfd);
 	}
 	bzero(&serv_addr, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = htons(INADDR_ANY);
-	serv_addr.sin_port = htons(10550); //Listening port
+	serv_addr.sin_port = htons(10500); //Listening port
 
-	bind(server_socket, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+	bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
 
-	listen(server_socket, 5);
-	
+	listen(listenfd, 2);
+
+	// if ((ns1 = accept(listenfd, (struct sockaddr *) NULL, NULL)) < 0) {
+	// perror("accept ns1");
+	// exit(1);
+	// }
+
+	// /* Accept another connection. */
+	// if ((ns2 = accept(listenfd, (struct sockaddr *) NULL, NULL)) < 0) {
+	// perror("accept ns2");
+	// exit(1);
+	// }
+
+
+	// while(1) {
+	// 	FD_ZERO(&fds);
+	// 	FD_SET(ns1, &fds);
+	// 	FD_SET(ns2, &fds);
+	// 	reader = select(3, &fds, 0, 0, 0);
+	// 	if ( FD_ISSET(ns1, &fds)) {
+	// 		nread = recv(ns1, buffer, sizeof(buffer), 0);
+	// 		if (nread < 1) { //Error checking
+	// 			printf("Read err\n");
+	// 			close(ns1);
+	// 			close(ns2);
+	// 			exit(-1);
+	// 		} 
+	// 		send(ns2, buffer, nread, 0);
+	// 	}
+	// 	if ( FD_ISSET(ns2, &fds)) {
+	// 		nread = recv(ns2, buffer, sizeof(buffer), 0);
+	// 		if (nread < 1) {
+	// 			close(ns1);
+	// 			close(ns2);
+	// 			exit(-1);
+	// 		}
+	// 		send(ns1, buffer, nread, 0);
+	// 	}
+
+
+	// }
+	connfd = accept(listenfd, (struct sockaddr*) NULL, NULL);
 	while(1) {
-		connfd = accept(server_socket, (struct sockaddr*) NULL, NULL);
+
 		if (connfd < 0) {
 			printf("Cannot connect");
 		}
+
 		int length = read(connfd, buffer, 1024);
 		if (length < 0) {
 			printf("errno %d\n", errno);
 			printf("%d\n", length);
 		}
+		if (strcmp(buffer, "end") == 0) {
+			printf("Command rcd'v from client. Terminating session.\n");
+			exit(0);
+		}
 		printf("Client message: %s\n", buffer);
 		write(connfd, buffer, sizeof(buffer));
-		close(connfd);
+		//bzero(buffer, sizeof(buffer));
+		//close(connfd);
 	}
-	close(server_socket);
+	close(listenfd);
 
 }
