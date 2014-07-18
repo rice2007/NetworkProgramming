26a5158bfc494f9cf45efd7503b0b412c7cd2a19
Ajay <rice2007@gmail.com>
2014-07-18 21:47:11 +0000

    Update aaronRc3.c

--- NetProg/aaronRc3.c
+++ NetProg/aaronRc3.c
@@ -13,13 +13,14 @@
 int main(int argc, char* argv[]) {
 
 	char buffer[1024];
-	int clientfd, sendErr, byteSize;
+	int clientfd, sendErr, byteSize, optInt;
+	int on = 1, off = 0, i = 0;
 	unsigned int fromLength;
 	struct hostend *hp;
 	struct sockaddr_in clientAddr, fromAddr, serverAddr;
 
-	if (argc != 2) {
-		printf("Incorrect args: <server IP> <port>");
+	if (argc != 3) {
+		printf("Incorrect args: <server IP> <port>\n");
 		exit(-1);
 	}
 	clientfd =  socket(AF_INET, SOCK_DGRAM, 0);
@@ -36,8 +37,17 @@ int main(int argc, char* argv[]) {
 		exit(-1);
 	}
 	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_addr.s_addr = inet_addr(argv[1]);
 	serverAddr.sin_port = htons(atoi(argv[2]));
+	optInt = setsockopt(clientfd, SOL_SOCKET, SO_BROADCAST, &on, 4);
+	serverAddr.sin_addr.s_addr |= htonl(0x1ff);
+	printf("bcast address: %s\n", inet_ntoa(serverAddr.sin_addr));
+	optInt = setsockopt(clientfd, SOL_SOCKET, SO_BROADCAST, &off, 4);
+	while (i < 10) {
+		recvfrom(clientfd, buffer, 1024, 0, (struct  sockaddr*) &fromAddr,
+			&fromLength);
+		sleep(1);
+		i++;	
+	}
 	//memcpy();
 
 	while (1) {
