a08b97a99554020719c7308db075e08820bd4a6e
rice2007 <rice2007@gmail.com>
2014-07-18 12:26:48 -0500

    Finish up

--- NetProg/aaronRs3.c
+++ NetProg/aaronRs3.c
@@ -1,29 +1,52 @@
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <limits.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 int main(int argc, char* argv[]) {
 
 	char buffer[1024];
-	int byteSize, serverSocket;
+	int byteSize, sendErr, serverfd;
 	int on = 1, off = 0;
 	unsigned int fromLength;
-	struct hostend *hp;
 	struct sockaddr_in slint, fromAddr, serverAddr;
 
-	clientSocket =  socket(AF_INET, SOCK_DGRAM, 0);
-	hp = gethostbyname(argv[1]);
-	server.sin_family = AF_INET;
-	//Fix
-	memcpy();
-
-	server.sin_port=htons(4096);
+	serverfd =  socket(AF_INET, SOCK_DGRAM, 0);
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr=htonl(INADDR_ANY);
+	serverAddr.sin_port=htons(10500);
+	bind(serverfd, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+	//memcpy();
 
 	while(1) {
-		printf();
-		byteSize = recvfrom(serverSocket, buffer, 1024, 0, (struct sockaddr*) &fromAddr,
+		byteSize = recvfrom(serverfd, buffer, 1024, 0, (struct sockaddr *) &fromAddr,
 			&fromLength);
-		printf();
+		if (byteSize < 0) {
+			perror("recvfrom");
+		} else if (byteSize == 0) {
+			printf("Client %d has disconnected.", serverAddr.sin_addr.s_addr);
+		}
+		printf("%d bytes from IP %s (%s)\n", byteSize, inet_ntoa(serverAddr.sin_addr), buffer);
+		printf("Rec'd msg: %s\n", buffer);
 		if (!strcmp(buffer, "end")) {
-			break;
+			printf("Shutdown cmd rec'd.");
+			exit(0);
 		}
 		strcpy(buffer, "msg rec'd");
+		sendErr = sendto(serverfd, buffer, 1024, 0, (struct sockaddr *) &fromAddr, fromLength);
+		if (sendErr < 0) {
+			perror("sendto");
+		}
 	}
 }
